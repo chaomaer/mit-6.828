3b2a536136cbc48f3c27d76d00a528cf4a847e7e
chaomaer <1506174913@qq.com>
2018-02-08 15:31:12 +0800

    lab2 is done

---
+++ lab2/kern/monitor.c
@@ -0,0 +1,171 @@
+// Simple command-line kernel monitor useful for
+// controlling the kernel and exploring the system interactively.
+
+#include <inc/stdio.h>
+#include <inc/string.h>
+#include <inc/memlayout.h>
+#include <inc/assert.h>
+#include <inc/x86.h>
+
+#include <kern/console.h>
+#include <kern/monitor.h>
+#include <kern/kdebug.h>
+#include <kern/pmap.h>
+#define CMDBUF_SIZE	80	// enough for one VGA text line
+
+
+struct Command {
+	const char *name;
+	const char *desc;
+	// return -1 to force monitor to exit
+	int (*func)(int argc, char** argv, struct Trapframe* tf);
+};
+
+static struct Command commands[] = {
+	{ "help", "Display this list of commands", mon_help },
+	{ "kerninfo", "Display information about the kernel", mon_kerninfo },
+    { "backtrace","Display the function call frame", backtrace},
+    { "showmappings", "show mappings from vaddr to paddr", showmappings}
+};
+
+/***** Implementations of basic kernel monitor commands *****/
+
+int
+mon_help(int argc, char **argv, struct Trapframe *tf)
+{
+	int i;
+
+	for (i = 0; i < ARRAY_SIZE(commands); i++)
+		cprintf("%s - %s\n", commands[i].name, commands[i].desc);
+	return 0;
+}
+
+int
+mon_kerninfo(int argc, char **argv, struct Trapframe *tf)
+{
+	extern char _start[], entry[], etext[], edata[], end[];
+
+	cprintf("Special kernel symbols:\n");
+	cprintf("  _start                  %08x (phys)\n", _start);
+	cprintf("  entry  %08x (virt)  %08x (phys)\n", entry, entry - KERNBASE);
+	cprintf("  etext  %08x (virt)  %08x (phys)\n", etext, etext - KERNBASE);
+	cprintf("  edata  %08x (virt)  %08x (phys)\n", edata, edata - KERNBASE);
+	cprintf("  end    %08x (virt)  %08x (phys)\n", end, end - KERNBASE);
+	cprintf("Kernel executable memory footprint: %dKB\n",
+		ROUNDUP(end - entry, 1024) / 1024);
+	return 0;
+}
+int
+showmappings(int argc, char **argv, struct Trapframe *tf)
+{
+	// check arg;
+	if(argc != 3){
+		cprintf("error: must have three args, like: showmappings addr1 addr2\n");
+        return -1;
+	}
+
+	int start = strtol(argv[1], 0, 16);
+	int end = strtol(argv[2], 0, 16);
+
+	// align
+	start = ROUNDDOWN(start, PGSIZE);
+	end = ROUNDDOWN(end, PGSIZE);
+
+	int i = start;
+	for (; i <= end; i += PGSIZE) {
+		cprintf("vaddr is %08x. ", i);
+		pte_t *pt_entry = 0;
+		pt_entry = pgdir_walk(kern_pgdir, (void*)i, 1);
+		if (pt_entry==NULL||!(*pt_entry&PTE_P)){
+			cprintf("not mapped\n");
+			continue;
+		}
+		int pyadd = PTE_ADDR(*pt_entry);
+		cprintf("paddr is %08x, PTE_W is %d,PTE_U is %d,PTE_P is %d\n", pyadd, *pt_entry&PTE_W, *pt_entry&PTE_U, *pt_entry&PTE_P);
+	}
+	return 0;
+}
+int
+backtrace(int argc, char **argv, struct Trapframe *tf)
+{
+  uint32_t* ebp = (uint32_t*) read_ebp();
+  cprintf("Stack backtrace:\n");
+  while (ebp) {
+    uint32_t eip = ebp[1];
+    cprintf("ebp %x  eip %x  args", ebp, eip);
+    int i;
+    for (i = 2; i <= 6; ++i)
+      cprintf(" %08.x", ebp[i]);
+    cprintf("\n");
+    struct Eipdebuginfo info;
+    debuginfo_eip(eip, &info);
+    cprintf("\t%s:%d: %.*s+%d\n", 
+      info.eip_file, info.eip_line,
+      info.eip_fn_namelen, info.eip_fn_name,
+      eip-info.eip_fn_addr);
+//         kern/monitor.c:143: monitor+106
+    ebp = (uint32_t*) *ebp;
+  }
+  return 0;
+}
+
+
+/***** Kernel monitor command interpreter *****/
+
+#define WHITESPACE "\t\r\n "
+#define MAXARGS 16
+
+static int
+runcmd(char *buf, struct Trapframe *tf)
+{
+	int argc;
+	char *argv[MAXARGS];
+	int i;
+
+	// Parse the command buffer into whitespace-separated arguments
+	argc = 0;
+	argv[argc] = 0;
+	while (1) {
+		// gobble whitespace
+		while (*buf && strchr(WHITESPACE, *buf))
+			*buf++ = 0;
+		if (*buf == 0)
+			break;
+
+		// save and scan past next arg
+		if (argc == MAXARGS-1) {
+			cprintf("Too many arguments (max %d)\n", MAXARGS);
+			return 0;
+		}
+		argv[argc++] = buf;
+		while (*buf && !strchr(WHITESPACE, *buf))
+			buf++;
+	}
+	argv[argc] = 0;
+
+	// Lookup and invoke the command
+	if (argc == 0)
+		return 0;
+	for (i = 0; i < ARRAY_SIZE(commands); i++) {
+		if (strcmp(argv[0], commands[i].name) == 0)
+			return commands[i].func(argc, argv, tf);
+	}
+	cprintf("Unknown command '%s'\n", argv[0]);
+	return 0;
+}
+
+void
+monitor(struct Trapframe *tf)
+{
+	char *buf;
+
+	cprintf("Welcome to the JOS kernel monitor!\n");
+	cprintf("Type 'help' for a list of commands.\n");
+
+	while (1) {
+		buf = readline("K> ");
+		if (buf != NULL)
+			if (runcmd(buf, tf) < 0)
+				break;
+	}
+}
